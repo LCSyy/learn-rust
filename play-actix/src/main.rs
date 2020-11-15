ae5e675a1a5ab08b9ae01957bcd4e9bce6b58d7c
chuns <chuns.liu@foxmail.com>
2020-11-15 23:04:42 +0800

    add sqlx

--- play-actix/src/main.rs
+++ play-actix/src/main.rs
@@ -3,13 +3,18 @@ mod service;
 
 use conf::{Config};
 use actix_web::{HttpServer, App};
+use sqlx::postgres::PgPoolOptions;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let config = Config::from_file("conf.toml");
     
-    HttpServer::new(||{
-        App::new().configure(service::service_config)
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgresql://postgres:pg2020@localhost:5432/postgres").await.unwrap();
+
+    HttpServer::new(move||{
+        App::new().data(pool.clone()).configure(service::service_config)
     })
     .bind(config.server.addr)?
     .run()
--- play-actix/src/service.rs
+++ play-actix/src/service.rs
@@ -1,8 +1,9 @@
 use actix_web::{
     Scope, Route, Responder, HttpResponse, 
-    web::{ServiceConfig},
+    web::{self, ServiceConfig},
     http::{Method}
 };
+use sqlx::postgres::PgPool;
 
 mod user;
 mod blog;
@@ -26,8 +27,13 @@ pub fn service_config(cfg: &mut ServiceConfig) {
     );
 }
 
-async fn login() -> impl Responder {
-    HttpResponse::Ok().body("Hello")
+async fn login(db: web::Data<PgPool>) -> impl Responder {
+
+    let row: (i64,)= sqlx::query_as("SELECT $1")
+        .bind(150i64)
+        .fetch_one(&*db.into_inner()).await.unwrap();
+
+    HttpResponse::Ok().body(format!("Hello {}", row.0))
 }
 
 async fn logout() -> impl Responder {
